c8809acbfe15b4ca07f741303a49ff5553fa44f8
CryingN <yihanyuedu@qq.com>
2023-11-12 21:24:55 +0800

    加了mygo题

---
+++ mygo-crypto/exp.py
@@ -0,0 +1,16 @@
+from pwn import *
+ciphertext = []
+p = remote("47.113.145.248" , 32123)
+# Send 40 null bytes (flag is 40 characters long)
+p.sendlineafter(": ","00"*40)
+for i in range(256):
+	p.recvuntil(": ")
+	ciphertext.append(p.recvuntil('\n')[:-1])
+
+p.recvuntil("Flag:  ")
+flag = p.recvuntil('\n')[:-1]
+# XOR all ciphertext with the encrypted flag 
+for c in ciphertext:
+	f = xor(bytes.fromhex(c.decode()),bytes.fromhex(flag.decode()))
+	if f.startswith(b"VYctf{"):
+		print(f)
